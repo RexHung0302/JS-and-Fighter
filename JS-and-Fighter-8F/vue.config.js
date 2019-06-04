ee648c6f33979ee59bed4648e92dae051ad7f561
ZeHung <zehung860486@gmail.com>
2019-06-04 13:46:54 +0800

    fix.

--- JS-and-Fighter-8F/vue.config.js
+++ JS-and-Fighter-8F/vue.config.js
@@ -2,8 +2,12 @@ module.exports = {
     publicPath: '/JS-and-Fighter/JS-and-Fighter-8F/dist/',
     configureWebpack: {
         performance: {
-            maxEntrypointSize: 512000,
-            maxAssetSize: 512000
+            maxEntrypointSize: 50000000,
+            maxAssetSize: 30000000,
+            assetFilter: function(assetFilename) {
+                // 提供资源文件名的断言函数
+                return assetFilename.endsWith('.css') || assetFilename.endsWith('.js');
+            }
         }
     }
 };
