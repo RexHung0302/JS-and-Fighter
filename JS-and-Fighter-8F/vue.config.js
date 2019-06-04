ab01b85a5eb10c36f701f6c407d7f2cf5ca9fb02
ZeHung <zehung860486@gmail.com>
2019-06-04 13:38:11 +0800

    fix.

--- JS-and-Fighter-8F/vue.config.js
+++ JS-and-Fighter-8F/vue.config.js
@@ -1,3 +1,9 @@
 module.exports = {
-    publicPath: '/JS-and-Fighter/JS-and-Fighter-8F/dist/'
+    publicPath: '/JS-and-Fighter/JS-and-Fighter-8F/dist/',
+    configureWebpack: {
+        performance: {
+            maxEntrypointSize: 512000,
+            maxAssetSize: 512000
+        }
+    }
 };
